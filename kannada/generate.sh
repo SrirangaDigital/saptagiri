c934b8ccb0f7be14f4941286f4c5d6f6ed3822c2
suresh07 <soori_rozee@yahoo.com>
2017-10-21 15:20:16 +0530

    ProjectL: initial commit

---
+++ kannada/generate.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+language="kannada"
+host="localhost"
+db="saptagiri_"$language
+usr="root"
+pwd="mysql"
+
+echo "CREATE DATABASE IF NOT EXISTS $db CHARACTER SET utf8 COLLATE utf8_general_ci;" | /usr/bin/mysql -u$usr -p$pwd
+
+perl $language"/insert_author.pl" $host $db $usr $pwd $language
+perl $language"/insert_feat.pl" $host $db $usr $pwd $language
+perl $language"/insert_articles.pl" $host $db $usr $pwd $language
+perl $language"/ocr.pl" $host $db $usr $pwd $language
